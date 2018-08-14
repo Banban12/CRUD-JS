b0744b81c7fc45dd46498419cbe65230fd526e27
Banban12 <banadraind@gmail.com>
2018-08-14 21:50:11 +0800

    Commit 5: Successfully Added new item to database using add button

--- routes/items.js
+++ routes/items.js
@@ -6,7 +6,6 @@ var app = express()
 app.get('/', function(req, res, next) {
     req.getConnection(function(error, conn) {
         conn.query('SELECT * FROM items ORDER BY id DESC',function(err, rows, fields) {
-            //if(err) throw err
             if (err) {
                 req.flash('error', err)
                 res.render('item/list', {
@@ -25,7 +24,6 @@ app.get('/', function(req, res, next) {
  
 // SHOW ADD USER FORM
 app.get('/add', function(req, res, next){	
-	// render to views/user/add.ejs
 	res.render('item/add', {
 		title: 'Add New Item',
 		id: '',
@@ -34,6 +32,73 @@ app.get('/add', function(req, res, next){
         amount: ''		
 	})
 })
+
+// ADD NEW USER POST ACTION
+app.post('/add', function(req, res, next){	
+	req.assert('id', 'ID is required').notEmpty()           
+	req.assert('name', 'Nam is required').notEmpty()             
+    req.assert('qty', 'QTY is required').notEmpty()  
+    req.assert('amount', 'Amount is required').notEmpty()  
+
+    var errors = req.validationErrors()
+    
+    if( !errors ) {  
+		
+	
+		var user = {
+			id: req.sanitize('id').escape().trim(),
+			name: req.sanitize('name').escape().trim(),
+            qty: req.sanitize('qty').escape().trim(),
+            amount: req.sanitize('amount').escape().trim()
+		}
+		
+		req.getConnection(function(error, conn) {
+			conn.query('INSERT INTO items SET ?', user, function(err, result) {
+				//if(err) throw err
+				if (err) {
+					req.flash('error', err)
+					
+					
+					res.render('item/add', {
+						title: 'Add New User',
+						id: user.id,
+						name: user.name,
+                        qty: user.qty,
+                        amount: user.amount					
+					})
+				} else {				
+					req.flash('success', 'Data added successfully!')
+					
+					
+					res.render('item/add', {
+						title: 'Add New User',
+						id: '',
+						name: '',
+                        qty: '',
+                        amount: ''					
+					})
+				}
+			})
+		})
+	}
+	else {  
+		var error_msg = ''
+		errors.forEach(function(error) {
+			error_msg += error.msg + '<br>'
+		})				
+		req.flash('error', error_msg)		
+		
+		
+        res.render('item/add', { 
+            title: 'Add New User',
+            id: req.body.id,
+            name: req.body.name,
+            qty: req.body.qty,
+            amount: req.body.amount
+        })
+    }
+})
+
 
 
 
